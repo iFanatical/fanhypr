ac9b95e8741376f1f903f1232573810469cb8156
Aaron Bush <theawpportunist@gmail.com>
2026-09-07 01:18:15 -0700

    Adjusted configuration for durandal.lua, as monitor positioning changed.

--- settings/monitors/durandal.lua
+++ settings/monitors/durandal.lua
@@ -1,5 +1,5 @@
-hl.monitor({ output = "DP-3", mode = "1920x1080@240", position = "auto", scale = "1", cm = "auto" })
-hl.monitor({ output = "DP-1", mode = "2560x1440@165", position = "auto", scale = "1", cm = "auto" })
+hl.monitor({ output = "DP-3", mode = "1920x1080@165", position = "-1920x0", scale = "1", cm = "auto" })
+hl.monitor({ output = "DP-1", mode = "2560x1440@165", position = "0x0", scale = "1", cm = "auto" })
 
 -- fallback
 hl.monitor({ output = "", mode = "preferred", position = "auto", scale = "auto" })
