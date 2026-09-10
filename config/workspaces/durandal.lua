428f5b81b79eea379b1a7a04e49ded5f4c9b1637
Aaron Bush <theawpportunist@gmail.com>
2026-09-10 16:56:05 -0700

    Adjusted durandal.lua workspaces to open on the appropriate monitors.

--- config/workspaces/durandal.lua
+++ config/workspaces/durandal.lua
@@ -1,7 +1,7 @@
 for workspace = 1, 10 do
     hl.workspace_rule({
         workspace = workspace,
-        monitor = workspace <= 5 and "DP-3" or "DP-1",
+        monitor = workspace <= 5 and "DP-1" or "DP-3",
         default = workspace == 1,
     })
 end
